aef7f4495e0f7772a85b1a44bfb34786fea135f3
kisky3 <kisky3@126.com>
2020-11-25 09:00:36 +0900

    画像のslide

--- linebot/src/components/ImagesSlide.vue
+++ linebot/src/components/ImagesSlide.vue
@@ -13,34 +13,11 @@
       @sliding-start="onSlideStart"
       @sliding-end="onSlideEnd"
     >
-      <!-- Text slides with image -->
-      <b-carousel-slide
-        caption="First slide"
-        text="Nulla vitae elit libero, a pharetra augue mollis interdum."
-        img-src="https://picsum.photos/1024/480/?image=52"
-      ></b-carousel-slide>
-
-      <!-- Slides with custom text -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=54">
-        <h1>Hello world!</h1>
-      </b-carousel-slide>
-
-      <!-- Slides with image only -->
-      <b-carousel-slide img-src="https://picsum.photos/1024/480/?image=58"></b-carousel-slide>
-
-      <!-- Slides with img slot -->
-      <!-- Note the classes .d-block and .img-fluid to prevent browser default image alignment -->
-      <b-carousel-slide>
-        <template #img>
-          <img
-            class="d-block img-fluid w-100"
-            width="1024"
-            height="480"
-            src="https://picsum.photos/1024/480/?image=55"
-            alt="image slot"
-          />
-        </template>
-      </b-carousel-slide>
+    <div  v-for="(img, index) in images"
+      :key="index">
+       <!-- Slides with image only -->
+      <b-carousel-slide :img-src="img"></b-carousel-slide>
+    </div>
     </b-carousel>
   </div>
 </template>
