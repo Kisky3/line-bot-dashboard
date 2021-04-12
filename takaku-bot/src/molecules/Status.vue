db776391cd64a2de0c26a9746670c999df4144da
kisky3 <kisky3@126.com>
2021-04-12 23:19:29 +0900

    :+1: statusを別componentにする

---
+++ takaku-bot/src/molecules/Status.vue
@@ -0,0 +1,19 @@
+<template>
+ <div>
+   status
+ </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "Status",
+  props: ["replied", "status"],
+  methods: {
+  }
+});
+</script>
+<style lang="scss">
+
+</style>
